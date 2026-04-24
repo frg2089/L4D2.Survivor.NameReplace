29f36c846078d1804afda42bf9505acec1d7a228
舰队的偶像-岛风酱! <frg2089@outlook.com>
2026-04-24 12:08:16 +0800

    feat: 添加自动打开文件功能
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/L4D2.Survivor.NameReplace/TemplateCommand.cs
+++ src/L4D2.Survivor.NameReplace/TemplateCommand.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 using DotMake.CommandLine;
 
 namespace L4D2.Survivor.NameReplace;
@@ -10,14 +12,19 @@ internal sealed class TemplateCommand
 
     public async Task RunAsync()
     {
-        await using var writer = SurvivorNames.CreateText();
-        await writer.WriteLineAsync("Rochelle = Rochelle");
-        await writer.WriteLineAsync("Coach = Coach");
-        await writer.WriteLineAsync("Ellis = Ellis");
-        await writer.WriteLineAsync("Nick = Nick");
-        await writer.WriteLineAsync("Bill = Bill");
-        await writer.WriteLineAsync("Zoey = Zoey");
-        await writer.WriteLineAsync("Francis = Francis");
-        await writer.WriteLineAsync("Louis = Louis");
+        await using (var writer = SurvivorNames.CreateText())
+        {
+            await writer.WriteLineAsync("Rochelle = Rochelle");
+            await writer.WriteLineAsync("Coach = Coach");
+            await writer.WriteLineAsync("Ellis = Ellis");
+            await writer.WriteLineAsync("Nick = Nick");
+            await writer.WriteLineAsync("Bill = Bill");
+            await writer.WriteLineAsync("Zoey = Zoey");
+            await writer.WriteLineAsync("Francis = Francis");
+            await writer.WriteLineAsync("Louis = Louis");
+        }
+
+        using var process = Process.Start(SurvivorNames.FullName);
+        process?.WaitForExit();
     }
 }
