ab354aa2de816aa86f28d42fdb44af94ca4df14c
舰队的偶像-岛风酱! <frg2089@outlook.com>
2026-04-24 12:06:11 +0800

    feat: 添加求生者名模板生成命令
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/L4D2.Survivor.NameReplace/TemplateCommand.cs
@@ -0,0 +1,23 @@
+using DotMake.CommandLine;
+
+namespace L4D2.Survivor.NameReplace;
+
+[CliCommand(Description = "创建求生者名模板", Parent = typeof(RootCommand))]
+internal sealed class TemplateCommand
+{
+    [CliOption(Description = "求生者名称模板文件")]
+    public FileInfo SurvivorNames { get; set; } = new(Path.Combine(AppContext.BaseDirectory, "SurvivorNames.ini"));
+
+    public async Task RunAsync()
+    {
+        await using var writer = SurvivorNames.CreateText();
+        await writer.WriteLineAsync("Rochelle = Rochelle");
+        await writer.WriteLineAsync("Coach = Coach");
+        await writer.WriteLineAsync("Ellis = Ellis");
+        await writer.WriteLineAsync("Nick = Nick");
+        await writer.WriteLineAsync("Bill = Bill");
+        await writer.WriteLineAsync("Zoey = Zoey");
+        await writer.WriteLineAsync("Francis = Francis");
+        await writer.WriteLineAsync("Louis = Louis");
+    }
+}
